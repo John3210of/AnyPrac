5b7647143d539e627d0f21954628c5592ef29a05
John <john3210of@gmail.com>
2021-12-05 09:32:51 +0900

    211205

---
+++ codingtest.py
@@ -0,0 +1,46 @@
+
+
+
+
+# chr는 정수>문자 // ord는 문자>정수 해주는 함수이다.
+
+#6044
+# a,b = map(int,input().split())
+# ##########map은 여러개 한번에 지정해줌
+# if(b==0):
+#     exit()
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a//b)
+# print(a%b)
+# print(format(a/b,".2f"))
+############### a<<b 는 2의 b승 // a>>b는 2의 -b승
+
+
+# a,b=map(int,input().split())
+#
+# if bool(a) and bool(not b) is True:
+#     print(True)
+# elif bool(not a) and bool(b) is True:
+#     print(True)
+# else:
+#     print(False)
+
+########### 3항연산
+# result = (a if a<b else b) if ((a if a<b else b)<c) else c
+# print(result)
+
+### ord 연산자 = 유니코드값 반환
+
+# for문에서 range(x,y) 일때 y가 x보다 작으면 출력하지 않음.
+# 하려면 range(10,0 ,-1) 이런식으로 음수값을 정해주어야 가능
+
+# x = int(input(),16)
+#
+#
+# for j in range(1,17):
+#     print('%X*%X=%X'%(x,j,x*j))
+#
+
+
