446b3f3b17e442e6eb21c9c4059bf8a64fb75c8d
john <john3210of@gmail.com>
2022-01-25 00:56:13 +0900

    이코테 음료수 얼려먹기

---
+++ 2cote/iced_drink.py
@@ -0,0 +1,84 @@
+# # 음료수 얼려먹기
+# # 상하좌우끼리 붙어있는 경우 연결되어 cnt++
+iced = [[0, 0, 1, 1, 0],
+        [0, 1, 1, 0, 1],
+        [1, 0, 1, 0, 1],
+        [0, 1, 0, 1, 0]]
+
+# 상 하 좌 우
+
+dr = [-1, 1, 0, 0]
+dc = [0, 0, -1, 1]
+
+
+# print(len(iced[0]))
+
+def icecream(iced):
+    cnt = 0
+
+    def dfs(row, col):
+        if iced[row][col]:
+            return
+        iced[row][col] = 1
+        for i in range(4):  # 상하좌우 가야됨
+            nr = row + dr[i]
+            nc = col + dc[i]
+            if nr < 0 or nc < 0 or nr >= 4 or nc >= 5:  # 멈춰
+                continue
+            dfs(nr, nc)
+
+    for row in range(len(iced)):  # node 갯수 세는법 여길 몰랐다
+        for col in range(len(iced[0])):
+            node = iced[row][col]
+            if node == 0:
+                cnt += 1
+                dfs(row, col)
+
+    print('cnt: ', cnt)
+
+
+icecream(iced)
+
+#
+#     ##여기까지가 안보고 친거###########
+
+# 음료수 얼려먹기
+# 상하좌우끼리 붙어있는 경우 연결되어 cnt++
+
+
+# def icecream(iced):
+#     # 상 하 좌 우
+#     dr = [-1, 1, 0, 0]
+#     dc = [0, 0, -1, 1]
+#     cnt = 0
+#
+#     def dfs(row, col):
+#
+#         if row < 0 or col < 0 or row >= 4 or col >= 5 or iced[row][col] == 1:  # 멈춰
+#             return
+#
+#         iced[row][col] = 1
+#
+#         for i in range(4):  # 상하좌우 가야됨
+#             nr = row + dr[i]
+#             nc = col + dc[i]
+#             dfs(nr, nc)
+#         return
+#
+#     for row in range(len(iced)):  # node 갯수 세는법
+#         for col in range(len(iced[0])):
+#             node = iced[row][col]
+#             if node == 0:
+#                 cnt += 1
+#                 dfs(row, col)
+#
+#     print(cnt)
+#     return cnt
+#
+#
+# iced = [[0, 0, 1, 1, 0],
+#         [0, 0, 0, 1, 1],
+#         [1, 1, 1, 1, 1],
+#         [0, 0, 0, 0, 0]]
+#
+# icecream(iced)
--- chap12/recursive_prac.py
+++ chap12/recursive_prac.py
@@ -1,35 +1,72 @@
-graph = {
-    1: [2, 3, 4],
-    2: [5],
-    3: [5],
-    4: [],
-    5: [6, 7],
-    6: [],
-    7: [3],
-}
+# graph = {
+#     1: [2, 3, 4],
+#     2: [5],
+#     3: [5],
+#     4: [],
+#     5: [6, 7],
+#     6: [],
+#     7: [3],
+# }
+#
+#
+# def dfs_recursive(node, visited):
+#     # 방문처리
+#     print('append 전',visited)
+#     visited.append(node)
+#     print('append 후', visited)
+#
+#     # 인접 노드 방문
+#     for adj in graph[node]:
+#         print('node', node)
+#         print('graph노드: ',graph[node])
+#         print('adj: ',adj)
+#
+#         if adj not in visited:
+#             print('==========================')
+#             print('recursive adj,visited: ',adj,visited)
+#             print('==========================')
+#             dfs_recursive(adj, visited)
+#
+#     print('result',visited)
+#     print('*==========================*')
+#     return visited
+#
+# if __name__ == "__main__":
+#     dfs_recursive(1,[])
+def island_dfs_recursive(grid):
+    dx = [0, 0, 1, -1]
+    dy = [1, -1, 0, 0]
+    m = len(grid)
+    n = len(grid[0])
+    cnt = 0
+
+    def dfs_recursive(r, c):
+        if r < 0 or r >= m or c < 0 or c >= n or grid[r][c] != '1':
+            return
+
+        # 방문처리
+        grid[r][c] = '0'
+        for i in range(4):
+            dfs_recursive(r + dx[i], c + dy[i])
+        return
+
+    for r in range(m):
+        for c in range(n):
+            node = grid[r][c]
+            if node != '1':
+                continue
+
+            cnt += 1
+            dfs_recursive(r, c)
+    print(cnt)
+    return cnt
 
 
-def dfs_recursive(node, visited):
-    # 방문처리
-    print('append 전',visited)
-    visited.append(node)
-    print('append 후', visited)
 
-    # 인접 노드 방문
-    for adj in graph[node]:
-        print('node', node)
-        print('graph노드: ',graph[node])
-        print('adj: ',adj)
-
-        if adj not in visited:
-            print('==========================')
-            print('recursive adj,visited: ',adj,visited)
-            print('==========================')
-            dfs_recursive(adj, visited)
-
-    print('result',visited)
-    print('*==========================*')
-    return visited
-
-if __name__ == "__main__":
-    dfs_recursive(1,[])
+grid=[
+    ["1", "1", "1", "1", "0"],
+    ["1", "1", "0", "1", "0"],
+    ["1", "1", "0", "0", "0"],
+    ["0", "0", "0", "0", "0"]
+]
+island_dfs_recursive(grid)
