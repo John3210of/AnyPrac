7b38e609382cc7b4f0861ec68d379d6f3d037a82
john <john3210of@gmail.com>
2022-04-12 10:13:13 +0900

    test : 인텔리제이로 옮겼어용

---
+++ 6oj/make_1011_flymetotheAlpha.py
@@ -0,0 +1,12 @@
+# import sys
+# ## 입력부
+# input = sys.stdin.readline
+
+k = int(input())
+
+for i in range(k):
+    x, y = map(int, input().split())
+
+# 인텔리제이 커밋 테스트
+#     45>46     //      49>50
+#      46>49가 2번?
