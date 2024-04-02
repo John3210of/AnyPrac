9755616765488fd02e0338a3bcc09601b0678290
john3210of@gmail.com <john3210of@gmail.com>
2024-04-02 23:58:17 +0900

    0402

--- 9rogramers/high_kit/stack_queue.py
+++ 9rogramers/high_kit/stack_queue.py
@@ -46,3 +46,61 @@ def solution(s):
                 return False
             stack.pop()
     return not stack
+
+# 4. 프로세스
+from collections import deque
+def solution(priorities, location):
+    # A B C D E F
+    # 1 1 9 1 1 1
+    
+    #  C D E F A B
+    #  9 1 1 1 1 1
+    
+    # queue => fifo, 꺼내면서 최대값이 나올때까지 꺼내면서 다시 오른쪽으로 붙인다.
+    # index가 필요
+    # 최대값이 나오면 꺼내고 stack에 붙인다.
+    sorted_pri = sorted(priorities)
+    queue=deque()
+    stack=[]
+    for i,v in enumerate(priorities):
+        queue.append([i,v])
+    while queue:
+        temp=queue.popleft()
+        if temp[1] != sorted_pri[-1]:
+            queue.append(temp)
+        else:
+            sorted_pri.pop()
+            stack.append(temp)
+    for i in range(len(stack)):
+        if stack[i][0]==location:
+            return i+1
+
+# 5. 다리를 지나는 트럭
+from collections import deque
+def solution(bridge_length, weight, truck_weights):
+    # 다리의 길이 = 건너는데 소요되는 시간 = bridge_length
+    # 다리의 가용 무게 = weight
+    # 택시의 무게 = truck_weights[n]
+    # 다리로 들어갈 경우 => 다리가 비었거나, 다리에 있는 무게의합+다음트럭의 무게 <= 가용무게일때
+    # 다리에서 빠질경우 => 시간이 트럭이 다리에 들어간 시점으로부터 길이만큼이 지났을 때
+    finished=[]
+    trucks=len(truck_weights)
+    truck_weights = deque(truck_weights)
+    report=deque()
+    bridge=deque()
+    time=0
+    while len(finished)!=trucks:
+        time+=1
+        # 다리에 차가 있고, 뺄 경우를 상정
+        if report and time - report[0] >= bridge_length:
+            finished.append(report.popleft())
+            bridge.popleft()
+        # 다리에 차가없거나, 차가 들어갈 수 있는경우
+        if not bridge or sum(bridge) + truck_weights[0] <= weight:
+            bridge.append(truck_weights.popleft())
+            report.append(time)
+                
+    return time
+
+
+solution(2, 10, [7,4,5,6])
